e7f4306c974c3dccc68f638f7b42fdc5ef123162
Ignacio Ramírez Paulino <nacho@fing.edu.uy>
2024-05-01 12:23:09 -0300

    paparazzi

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from distutils.core import setup, Extension
+
+eca=["-fopenmp", "-O3", "-march=native", "-mtune=native"]
+#eca=["-ggdb", "-O0", "-march=native", "-mtune=native"]
+
+setup(name             = "patches",
+      version          = "1.0",
+      description      = "patch extraction and stitching.",
+      author           = "Ignacio Francisco Ramirez Paulino",
+      author_email     = "nacho@fing.edu.uy",
+      maintainer       = "nacho@fing.edu.uy",
+      url              = "https://iie.fing.edu.uy/personal/nacho/",
+      ext_modules      = [
+          Extension(
+              'patches', ['src/patches.c','src/mmap.c'],
+              extra_compile_args=eca)
+     ]
+)
